98c151b1a45cdaa8daee896bbfce096de76f0794
Christoph Gardner <1083542@apps.nsd.org>
2022-05-19 09:27:02 -0700

    .

--- script.js
+++ script.js
@@ -12,61 +12,47 @@ function guessingGame(){
   const yesDom      = document.getElementById("yes");
   const guessesDom  = document.getElementById("guesses");
   const bestDom     = document.getElementById("best");
-
-  let answer = -1; 
-  let tries  = 0;
+ 
   play();
 
   guessDom.addEventListener("change",makeGuess);
-  yesDom.addEventListener("click",play);
+
   
   function play(){
-    guessDom.setAttribute("placeholder", "1-100");
-    hintDom.innerHTML = "Give it a guess!";
-    guessDom.value = "";
-    guessingDom.style.display = "unset";
-    playingDom.style.display = "none";
+    alert("Let's play...");
+    let answer = Math.loor(Math.random() * 100) +1;
+    console.log("this is the document");
+    console.log(document);
+    console.log("this is thE form:: ");
+    console.log(document.getElementById("guessingForm"));
+  }
 
-    answer = Math.floor(Math.random() * 100) +1;
-    ansDom.value = answer;
-    tries = 1;
-  }
-  function setTries(soFar){
-    return soFar;
-  }
   function makeGuess(e){
-    this.setAttribute("placeholder",this.value);
+    alert("I guessed!");
+    console.log("This is this:");
+    console.log(this);
+    console.log("This is the event:");
+    console.log(e);
+    // updat count of guessEs!
+    while(this.value != answer){
+      if(this.value < answer){
+        alert("????");
+      }
+      else if (this.value > answer){
+      alert("greater?");
+    }
     
-    if      (this.value < answer) hintDom.innerHTML = "HIGHER! The answer is higher!";
-    else if (this.value > answer) hintDom.innerHTML = "LOWER! The answer is lower!";
-    else    showGameResults(tries);
-  
-    triesDom.value = tries++;
-    this.value = "";
-
-    setTimeout(function(){
-      guessDom.focus();
-    },10);
   }
 
   function showGameResults(tries){
-    guessingDom.style.display = "none";
-    playingDom.style.display  = "unset";
-    score.value = tries;
-    guessesDom.value = tries + parseInt(guessesDom.value);
-    bestDom.value = getBest(tries);
-    gamesDom.value = parseInt(gamesDom.value) + 1;
-    yesDom.focus();
+    alert("showGameResults");
   }
   
   function getBest(count){
-    const check = parseInt(bestDom.value);
-    if(check === 0 || count < check) return count;
-    else return check;
+    alert("getBest");
   }
   
 }
-
 
 
 guessingGame();
